b4d69826e6859bdf9f1930fda0f81177755cfd96
Anmol Gupta <guptaanmol184@gmail.com>
2024-05-10 09:14:12 +0530

    practice

---
+++ practice/takeuforward/array/03-check-if-array-is-sorted.py
@@ -0,0 +1,20 @@
+# LC: 1752: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+
+def check(self, nums: List[int]) -> bool:
+    wrong_order_count = 0
+    l = len(nums)
+    for i in range(l):
+        if nums[(i+1)% l] >= nums[i]:
+            continue
+        else:
+            wrong_order_count +=1 
+            if wrong_order_count > 1:
+                return False
+    
+    return True
+
+
+if __name__ == '__main__':
+    assert True == check([1,2,3,4,5])
+    assert False == check(3,4,5,1,2,4)
+    assert True == check(3,4,5,12)
